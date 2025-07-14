1285431afc41816725f8a7e72f0c663956a77b3b
Dhyey1811 <dhyeypatel9640@gmail.com>
2025-07-13 20:39:39 -0400

    ts file

--- lib/dhyey8965482-cdk-project-stack.ts
+++ lib/dhyey8965482-cdk-project-stack.ts
@@ -1,16 +1,39 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class Dhyey8965482CdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // ✅ S3 Bucket
+    const myBucket = new s3.Bucket(this, 'dhyey8965482Bucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Dhyey8965482CdkProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // ✅ Lambda Function
+    const myLambda = new lambda.Function(this, 'dhyey8965482Lambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          console.log("Lambda Triggered");
+          return { statusCode: 200, body: "Hello from dhyey8965482" };
+        };
+      `),
+      environment: {
+        BUCKET_NAME: myBucket.bucketName,
+      },
+    });
+
+    // ✅ DynamoDB Table
+    const myTable = new dynamodb.Table(this, 'dhyey8965482Table', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      tableName: 'dhyey8965482Table',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
